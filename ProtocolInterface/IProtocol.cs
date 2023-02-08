cc9501ad4c50328e8e72acf9d20390088718c4be
曹一梁 <caoyiliang@me.com>
2023-02-08 16:07:18 +0800

    添加项目文件。

---
+++ ProtocolInterface/IProtocol.cs
@@ -0,0 +1,32 @@
+﻿using Communication;
+
+namespace ProtocolInterface
+{
+    internal interface IProtocol
+    {
+        /// <summary>
+        /// 设备是否连接
+        /// </summary>
+        public bool IsConnect { get; }
+
+        /// <summary>
+        /// 打开串口
+        /// </summary>
+        Task OpenAsync();
+
+        /// <summary>
+        /// 关闭串口
+        /// </summary>
+        Task CloseAsync();
+
+        /// <summary>
+        /// 对端掉线
+        /// </summary>
+        event DisconnectEventHandler? OnDisconnect;
+
+        /// <summary>
+        /// 对端连接成功
+        /// </summary>
+        event ConnectEventHandler? OnConnect;
+    }
+}
