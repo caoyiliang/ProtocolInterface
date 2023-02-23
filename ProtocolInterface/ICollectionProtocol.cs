6051c3ca3aefddac7aea7dc2cf2b152b6422aa0d
曹一梁 <caoyiliang@me.com>
2023-02-23 14:31:46 +0800

    采集口变更

--- ProtocolInterface/ICollectionProtocol.cs
+++ ProtocolInterface/ICollectionProtocol.cs
@@ -5,6 +5,7 @@ namespace ProtocolInterface;
 public interface ICollectionProtocol : IProtocol
 {
     string Name { get; }
+    bool Active { get; }
 
     event ActivelyPushDataEventHandler<Dictionary<string, decimal>>? OnDevicePushData;
 }
