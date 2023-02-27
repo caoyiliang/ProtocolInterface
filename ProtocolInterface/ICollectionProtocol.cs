3361b976c5dd67af56bb9910e29bff8b25be466e
曹一梁 <caoyiliang@me.com>
2023-02-27 15:38:29 +0800

    更新依赖

--- ProtocolInterface/ICollectionProtocol.cs
+++ ProtocolInterface/ICollectionProtocol.cs
@@ -5,7 +5,6 @@ namespace ProtocolInterface;
 public interface ICollectionProtocol : IProtocol
 {
     string Name { get; }
-    bool Active { get; }
 
     event ActivelyPushDataEventHandler<Dictionary<string, decimal>>? OnDevicePushData;
 }
