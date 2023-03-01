5a0a487a5802b2a9d2c13643c2899f54e89ed5df
曹一梁 <caoyiliang@me.com>
2023-03-01 20:05:28 +0800

    更新接口

---
+++ ProtocolInterface/IDeviceProtocol.cs
@@ -0,0 +1,12 @@
+﻿using Communication;
+
+namespace ProtocolInterface;
+
+public interface IDeviceProtocol : IProtocol
+{
+    string Name { get; }
+
+    event ActivelyPushDataEventHandler<(DateTime, Dictionary<string, decimal>)>? OnDevicePushData;
+
+    Task SendData((DateTime, Dictionary<string, decimal> data) channelsDatas);
+}
