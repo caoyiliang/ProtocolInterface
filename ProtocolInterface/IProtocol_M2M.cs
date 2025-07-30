be423b5ce5f568308f3e6924324581218e55ffbf
曹一梁 <caoyiliang@me.com>
2025-07-30 14:43:55 +0800

    更新版本和新增 IProtocol_M2M 接口
    在 `ProtocolInterface.csproj` 文件中，版本号从 `9.10.2` 更新为 `9.10.3`，同时 `CSoft.TopPortLib` 的依赖版本也从 `9.15.2` 更新为 `9.15.3`。
    
    在 `IProtocol_M2M.cs` 文件中，新增了 `IProtocol_M2M` 接口，包含了设备监听状态、客户端连接事件、发送和接收数据事件，以及启动和停止监听的方法。此外，还新增了获取客户端信息的方法。

---
+++ ProtocolInterface/IProtocol_M2M.cs
@@ -0,0 +1,44 @@
+﻿using Communication;
+using TopPortLib;
+
+namespace ProtocolInterface;
+
+public interface IProtocol_M2M
+{
+    /// <summary>
+    /// 设备是否监听
+    /// </summary>
+    public bool IsListened { get; }
+
+    /// <summary>
+    /// 客户端连接事件
+    /// </summary>
+    event ClientConnectEventHandler OnClientConnect;
+
+    /// <summary>
+    /// 发送数据事件
+    /// </summary>
+    event RequestedLogServerEventHandler OnSentData;
+
+    /// <summary>
+    /// 收到数据事件
+    /// </summary>
+    event RespondedLogServerEventHandler OnReceivedData;
+
+    /// <summary>
+    /// 打开监听
+    /// </summary>
+    Task StartAsync();
+
+    /// <summary>
+    /// 关闭监听
+    /// </summary>
+    Task StopAsync();
+
+    /// <summary>
+    /// 获取客户端信息
+    /// </summary>
+    /// <param name="clientId">客户端ID</param>
+    /// <returns>客户端信息</returns>
+    Task<string?> GetClientInfos(Guid clientId);
+}
